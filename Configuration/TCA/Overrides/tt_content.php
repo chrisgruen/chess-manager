6f8cb4800d06e707607a1ef32532e79228a690f4
chrisgruen <cg@romonta-schach.de>
2020-11-29 08:29:53 +0100

    show plugin name

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,6 +3,6 @@
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'ChessManager',
     'ManageChessResults',
-    'The Store Team List',
+    'Chess manager',
     'EXT:chess_manager/Resources/Public/Icons/Extension.svg'
 );
